a8a8caaf8ee593bab870bffa4dd3b4868e9d2211
Rudy Jahchan <rudy@carbonfive.com>
2018-08-29 13:41:51 -0700

    Roll each dice

--- dicepool/check.go
+++ dicepool/check.go
@@ -2,6 +2,7 @@ package starwarsdice
 
 import (
 	"fmt"
+	"sync"
 )
 
 // Check represents the dice we need to roll to see if we accomplished a task.
@@ -16,28 +17,62 @@ type Check struct {
 }
 
 // Roll peforms the check and reports the result.
-func (c Check) Roll() Result {
-	var result Result
-	result = result.Add(roll(Ability, c.Ability))
-	result = result.Add(roll(Proficiency, c.Proficiency))
-	result = result.Add(roll(Boost, c.Boost))
-	result = result.Add(roll(Difficulty, c.Difficulty))
-	result = result.Add(roll(Challenge, c.Challenge))
-	result = result.Add(roll(Setback, c.Setback))
+func (c Check) Roll() {
+	done := make(chan Result)
+	rolls := make(chan Roll, 100)
+	dice := make(chan Die, 100)
+
+	go buildDicePool(c, dice)
+	go roll(dice, rolls)
+	go printAndDisplay(rolls, done)
+
+	result := <-done
 	fmt.Println(RESET, "\n\n", result)
-	return result
 }
 
-func roll(d Die, c int) Result {
+func buildDicePool(c Check, dice chan Die) {
+	buildDiePool(Ability, c.Ability, dice)
+	buildDiePool(Proficiency, c.Proficiency, dice)
+	buildDiePool(Boost, c.Boost, dice)
+	buildDiePool(Difficulty, c.Difficulty, dice)
+	buildDiePool(Challenge, c.Challenge, dice)
+	buildDiePool(Setback, c.Setback, dice)
+	close(dice)
+}
+
+func buildDiePool(d Die, count int, dice chan Die) {
+	for index := 0; index < count; index++ {
+		dice <- d
+	}
+}
+
+func roller(rollersGroup *sync.WaitGroup, dice chan Die, rolls chan Roll) {
+	defer rollersGroup.Done()
+	for die := range dice {
+		rolls <- die.Roll()
+	}
+}
+
+func roll(dice chan Die, rolls chan Roll) {
+	var rollersGroup sync.WaitGroup
+	for index := 0; index < 100; index++ {
+		rollersGroup.Add(1)
+		go roller(&rollersGroup, dice, rolls)
+	}
+	rollersGroup.Wait()
+	close(rolls)
+}
+
+func printAndDisplay(rolls chan Roll, finalResult chan Result) {
 	var result Result
-	for index := 0; index < c; index++ {
-		roll := d.Roll()
-		if index%40 == 0 {
+	var count uint
+	for roll := range rolls {
+		if count%40 == 0 {
 			fmt.Print("\n")
 		}
-		fmt.Print(d, " ")
+		fmt.Print(roll.Die, " ")
+		count++
 		result = result.Add(roll.Result)
 	}
-
-	return result
+	finalResult <- result
 }
